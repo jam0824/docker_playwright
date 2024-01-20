92ed67da6037ae1756a06bb0fa6d39b1a6923c97
jam0824 <session_fixation@hotmail.co.jp>
2024-01-20 11:14:33 +0900

    Readme追加

--- mount/start.sh
+++ mount/start.sh
@@ -1,4 +1,7 @@
 #!/bin/bash
+# 仮想ディスプレイを1920x1080x16で作って、後のコマンドを実行できるように&をつける
 Xvfb :10 -screen 0 1920x1080x16 &
+# x11vncを立ち上げてアクセスできるようにする。5910ポートでlocalhostのみ許可
 x11vnc -rfbport 5910 -forever -localhost &
+# websockifyでホスト側のブラウザで見れるようにする。8010ポートとlocalhostの5910をつなげている
 websockify --web=/usr/share/novnc 8010 localhost:5910 &
